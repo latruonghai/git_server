d523f65a6a49f10f569cd136b08ebd0315c29a0e
= <=>
2020-04-30 17:02:37 +0700

    eigth

--- GIThub/test.py
+++ GIThub/test.py
@@ -9,10 +9,10 @@ j_start = int(i_startJ_start[1])
 i_end = int(i_startJ_start[2])
 
 j_end = int(i_startJ_start[3])
-matrix2 = []
-for i in range(n):
-    matrix = [' ' for i in range(n)]
-    matrix2.append(matrix)
+#matrix2 = []
+#for i in range(n):
+    #matrix = [' ' for i in range(n)]
+    #matrix2.append(matrix)
 
 
 # print(matrix2)
@@ -30,8 +30,8 @@ def printShortestPath(n, i_start, j_start, i_end, j_end):
     #des = []
     #d2 = []
     c = []
-    matrix2[i_start][j_start] = 'x'
-    matrix2[i_end][j_end] = 'y'
+    #matrix2[i_start][j_start] = 'x'
+    #matrix2[i_end][j_end] = 'y'
     start = (i_start, j_start)
     end = (i_end, j_end)
     dis = distances(start, end)
@@ -62,14 +62,14 @@ def printShortestPath(n, i_start, j_start, i_end, j_end):
                     dis = Dist
                     break
             c.append(d[d2[i]])
-            if (0 <= j_start < n and 0 <= i_start < n):
-                matrix2[i_start][j_start] = str(len(c))
+            #if (0 <= j_start < n and 0 <= i_start < n):
+                #matrix2[i_start][j_start] = str(len(c))
             if dis<=1:
                 break
         else:
             break
-    for i in range(len(matrix2)):
-        print(matrix2[i])
+    #for i in range(len(matrix2)):
+        #print(matrix2[i])
     # print(matrix2)
     if dis == 0:
         print(len(c))
