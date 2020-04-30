ea3b6b056a3bf1b6716d779932eeea49d1b1e9fb
= <=>
2020-04-30 16:29:13 +0700

    seventh

--- GIThub/hackerrank.py
+++ GIThub/hackerrank.py
@@ -25,8 +25,8 @@ def distances(tup, tup1):
 
 
 def printShortestPath(n, i_start, j_start, i_end, j_end):
-    d = {(-2, -1): 'UL', (-2, 1): 'UR', (0, 2): 'R', (2, 1): 'LR', (2, -1): 'LL', (0, -2): 'L', (0, 0): 'Error'}
-    d1 = [(-2, -1), (-2, 1), (0, 2), (2, 1), (2, -1), (0, -2), (0, 0)]
+    d = {(-2, -1): 'UL', (-2, 1): 'UR', (0, 2): 'R', (2, 1): 'LR', (2, -1): 'LL', (0, -2): 'L'}
+    #d1 = [(-2, -1), (-2, 1), (0, 2), (2, 1), (2, -1), (0, -2)]
     #des = []
     #d2 = []
     c = []
@@ -37,22 +37,31 @@ def printShortestPath(n, i_start, j_start, i_end, j_end):
     dis = distances(start, end)
     # d = ((i_start-i_end)**2+(j_start-j_end)**2)**0.5
     while dis > 0:
-        if (0 <= j_start <= n and 0 <= i_start <= n):
+        if (0 <= j_start < n and 0 <= i_start < n):
             temp = start
-            dic = {}
-            for i in range(len(d1)):
-                # c1= d[d1[i]]
-                start = i_start, j_start = sum(temp, d1[i])
+            if (i_end<i_start and j_end<j_start):       #on the lowright of end
+                d2 = [(-2,-1),(0,-2)]                   #UL, L
+            elif (i_end>i_start and j_end<j_start):     #on the upright of end
+                d2 = [(2, -1),(0, -2)]                   #LL,L 
+            elif (i_end<i_start and j_end>j_start):      #on the lowleft of end
+                d2 = [(-2, 1),(0, 2)]                    #UR ,R
+            elif (i_end>i_start and j_end>j_start):     #on the upleft of end
+                d2 = [((2, 1),(0, 2))]                    #R, LR
+            elif (i_end>i_start and j_end==j_start):     #on the up-straight of end
+                d2 = [(2, -1)]
+            elif (i_end<i_start and j_end==j_start):     #on the low-height of end
+                d2 = [(-2, -1)]
+            elif (i_end==i_start and j_end<j_start):     #on the up-width of end
+                d2 = [(0, -2)]
+            elif (i_end==i_start and j_end>j_start):     #on the up-width of end
+                d2 = [(0, 2)]
+            for i in range(len(d2)):
+                start = i_start, j_start = sum(temp, d2[i])
                 Dist = distances(start, end)
                 if Dist < dis:
                     dis = Dist
                     break
-            if i == len(d1)-1:
-                print("Impossible")
-                break
-            else:
-            # temp5 = dic[dis]
-                c.append(d[d1[i]])
+            c.append(d[d2[i]])
             if (0 <= j_start < n and 0 <= i_start < n):
                 matrix2[i_start][j_start] = str(len(c))
         else:
