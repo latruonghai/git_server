7f2b148260abf585e2188cf40717b76b8dfb9db5
= <=>
2020-04-30 17:24:18 +0700

    final knight

--- GIThub/hackerrank.py
+++ GIThub/hackerrank.py
@@ -1,34 +1,19 @@
 n = int(input())
-
 i_startJ_start = input().split()
-
 i_start = int(i_startJ_start[0])
-
 j_start = int(i_startJ_start[1])
-
 i_end = int(i_startJ_start[2])
-
 j_end = int(i_startJ_start[3])
 matrix2 = []
 for i in range(n):
     matrix = [' ' for i in range(n)]
     matrix2.append(matrix)
-
-
-# print(matrix2)
 def sum(tup, tup1):
     return (tup[0] + tup1[0], tup[1] + tup1[1])
-
-
 def distances(tup, tup1):
     return ((tup[0] - tup1[0]) ** 2 + (tup[1] - tup1[1]) ** 2) ** 0.5
-
-
 def printShortestPath(n, i_start, j_start, i_end, j_end):
     d = {(-2, -1): 'UL', (-2, 1): 'UR', (0, 2): 'R', (2, 1): 'LR', (2, -1): 'LL', (0, -2): 'L'}
-    #d1 = [(-2, -1), (-2, 1), (0, 2), (2, 1), (2, -1), (0, -2)]
-    #des = []
-    #d2 = []
     c = []
     matrix2[i_start][j_start] = 'x'
     matrix2[i_end][j_end] = 'y'
@@ -68,12 +53,9 @@ def printShortestPath(n, i_start, j_start, i_end, j_end):
             break
     for i in range(len(matrix2)):
         print(matrix2[i])
-    # print(matrix2)
     if dis == 0:
         print(len(c))
         print(" ".join(c))
     else:
         print("Impossible")
-
-
 printShortestPath(n, i_start, j_start, i_end, j_end)
